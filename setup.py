ff132f76eb2b051caeb570bd7c5cff5196e4a751
Andrew Reusch <areusch@gmail.com>
2019-10-16 10:20:04 -0700

    Add pysh main entry point.

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(
   packages=['pysh', 'pysh.ipython'],
   setup_requires=['tox-setuptools'],
   tests_require=['more-itertools < 6.0.0', 'pipenv', 'six', 'tox'],
+  entry_points={
+    'console_scripts': ['pysh = pysh.__main__:main'],
+  },
 )
